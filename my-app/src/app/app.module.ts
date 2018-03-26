99a71c496dc58c7eaeee207fe6a980958aa54602
mess81 <mess1981@hotmail.fr>
2018-03-26 15:48:51 +0200

    generate components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
+import { PageErrorModule } from './page-error/page-error.module';
 
 import { AppComponent } from './app.component';
+
 
 
 
@@ -13,7 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    HomeModule,
+    PageErrorModule
   ],
   declarations: [
     AppComponent
